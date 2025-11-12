4afffd76cd846d94fe9d1328917fc5ffa1642aa8
Singh Anmol <anmol.singh@etu.unistra.fr>
2025-11-12 17:57:17 +0100

    formatJson

---
+++ fonction/formatJson.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE FUNCTION formatJson(id Image.idImage%TYPE) 
+RETURN CLOB IS
+
+json_o  CLOB; 
+
+BEGIN
+    SELECT JSON_OBJECT(
+        'idImage' VALUE i.idImage,
+        'titre' VALUE i.titre, 
+        'Description' VALUE i.description, 
+        'Auteur' VALUE u.nom, 
+        'Categorie' VALUE c.nom, 
+        'Nb_likes' VALUE COUNT(l.idUtilisateur)
+        ) 
+    into json_o 
+    FROM image i 
+    JOIN utilisateur u 
+    ON u.idUtilisateur = i.idUtilisateur 
+    JOIN categorie c 
+    ON i.idCategorie = c.idCategorie  
+    JOIN likes l 
+    ON l.idImage = i.idImage
+    WHERE i.idImage = id
+    GROUP BY i.idImage, i.titre, i.description, u.nom, c.nom; 
+
+
+    RETURN json_o; 
+
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RETURN '{"error": "Aucune image trouvée"}';
+
+END; 
+/ 
